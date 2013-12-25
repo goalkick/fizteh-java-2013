a2dea73a528076cc10436b4b902ed0101d7d8956
goalkick <goalkick94@gmail.com>
2013-12-26 02:27:50 +0400

    storeable

--- src/ru/fizteh/fivt/students/zhidkovanton/storable/DataFactoryProvider.java
+++ src/ru/fizteh/fivt/students/zhidkovanton/storable/DataFactoryProvider.java
@@ -4,28 +4,22 @@ import ru.fizteh.fivt.storage.structured.TableProvider;
 import ru.fizteh.fivt.storage.structured.TableProviderFactory;
 
 import java.io.File;
-import java.io.IOException;
 
 public class DataFactoryProvider implements TableProviderFactory {
 
     @Override
     public TableProvider create(String dir) {
-        try {
-            if ((dir == null) || dir.trim().equals("")) {
-                throw new IllegalArgumentException("Bad base directory");
+        if ((dir == null) || dir.trim().equals("")) {
+            throw new IllegalArgumentException("Bad base directory");
+        }
+        File directoryFile = new File(dir);
+        if (!directoryFile.exists()) {
+            if (!directoryFile.mkdir()) {
+                throw new IllegalArgumentException("Can't create" + dir);
             }
-            File directoryFile = new File(dir);
-            if (!directoryFile.exists()) {
-                if (!directoryFile.mkdir()) {
-                    throw new IOException("Can't create" + dir);
-                }
-            }
-            if (!directoryFile.isDirectory()) {
-                throw new IOException("Hmm... It isn't a directory" + dir);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            System.exit(1);
+        }
+        if (!directoryFile.isDirectory()) {
+            throw new IllegalArgumentException("Hmm... It isn't a directory" + dir);
         }
         return new DataFactory(dir);
     }
